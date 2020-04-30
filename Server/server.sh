f0e15a7ed8fc9749449b4dec0c6b724720ca17e7
Rizvi <rizvi22222@yahoo.com>
2020-04-29 23:37:30 -0700

    Create server.sh

---
+++ Server/server.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y collectd-dev
+sudo apt-get install -y bind9
+sudo chmod 777 /etc/collectd/
+scp /config/collectd.conf /etc/collectd/
+sudo chmod 777 /etc/bind/
+sudo scp /config/named.conf /etc/bind/
+cd /etc/bind/
+mkdir dynamic
+mkdir slaves
+sudo chmod 777 /etc/bind/slaves/
+sudo scp zone-files/* /etc/bind/slaves/
+sudo chmod 777 /etc/
+sudo scp /name-server/resolv.conf /etc/
+© 2020 GitHub, Inc.
